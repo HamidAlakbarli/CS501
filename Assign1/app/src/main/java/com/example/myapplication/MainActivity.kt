bd2bc3df798a522c99582bb36e2ec20b11f082ff
Hamid Alakbarli <hamidalakbarli@crc-dot1x-nat-10-239-186-206.bu.edu>
2025-01-27 18:30:17 -0500

    Button Added

--- Assign1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Assign1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,45 +3,37 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "World",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MyScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyScreen() {
+    val message = remember { mutableStateOf("") } // State to hold the text
+
+    Column {
+        Text(text = message.value) // Display the message
+        Button(onClick = { message.value = "Hello World!" }) {
+            Text("Click Me") // Button text
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("World")
-    }
+fun MyScreenPreview() {
+    MyScreen()
 }
